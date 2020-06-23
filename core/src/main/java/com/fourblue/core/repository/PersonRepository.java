5e9cb9e40b0ab7b95aaf2d508395ec719e098047
deborasandi <deborasandi@users.noreply.github.com>
2020-06-23 19:35:59 -0300

    troquei a conexão com o bd

--- core/src/main/java/com/fourblue/core/repository/PersonRepository.java
+++ core/src/main/java/com/fourblue/core/repository/PersonRepository.java
@@ -5,6 +5,5 @@ import com.fourblue.core.model.Person;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 }
--- project/src/main/java/com/fourblue/project/ProjectApplication.java
+++ project/src/main/java/com/fourblue/project/ProjectApplication.java
@@ -2,10 +2,13 @@ package com.fourblue.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("com.fourblue.core")
+@EntityScan({"com.fourblue.core.model"})
+@EnableJpaRepositories({"com.fourblue.core.repository"})
 public class ProjectApplication {
 
     public static void main(String[] args) {
