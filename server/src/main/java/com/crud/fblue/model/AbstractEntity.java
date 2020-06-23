8e70f00277fbd4d68520474b745441b9a379ac68
deborasandi <deborasandi@users.noreply.github.com>
2020-06-23 14:13:05 -0300

    removendo tudo

--- server/src/main/java/com/crud/fblue/model/AbstractEntity.java
+++
@@ -1,7 +0,0 @@
-package com.crud.fblue.model;
-
-import java.io.Serializable;
-
-public interface AbstractEntity extends Serializable {
-    Long getId();
-}
--- server/src/main/java/com/crud/fblue/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crud.fblue.repository;
-
-import com.crud.fblue.model.Person;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
-}
